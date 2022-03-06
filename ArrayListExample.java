dd17a26a883704a3162334a75756381a886787f6
thejoker429 <97645439+thejoker429@users.noreply.github.com>
2022-03-06 13:33:02 -0500

    Add files via upload
    ArrayListExample with JUnit Testing file. This will be a part of the integrated and continuous workflow I am working on.

---
+++ ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please enter a name to be output: ");
+		String output = input.nextLine();
+		System.out.println(output);
+		
+	}
+	
+}
---
+++ JUnit_Test.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JUnit_Test {
+
+	@Test
+	public void test() {
+		
+		System.out.println("This is a failed test");
+		String test = "This is a test";
+		assertEquals("This is a failed test", test);
+		
+	}
+
+}
